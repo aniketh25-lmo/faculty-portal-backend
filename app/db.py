d31fb28d0efabc940ebd8390e1e1553cac36ebb8
aniketh25-lmo <anikethtirupate25@gmail.com>
2026-02-14 02:24:07 +0530

    changed db.py again

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 import os
 from supabase import create_client
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
